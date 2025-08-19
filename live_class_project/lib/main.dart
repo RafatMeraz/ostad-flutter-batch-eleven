923a25ff98895519885d103cc54e77678b6d547f
RafatMeraz <razhossain201@gmail.com>
2025-08-19 23:03:28 +0600

    Add product CRUD app's ui

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,95 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:live_class_project/screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(CrudApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
-  }
-}
-
-// Mutatable - Changeable
-// Immutable - Static
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return HomeScreenState();
-  }
-}
-
-class HomeScreenState extends State<HomeScreen> {
-  int count = 0;
-  final TextEditingController _textEditingController = TextEditingController();
-
-  // Important
-  @override
-  void initState() {
-    super.initState();
-    print('Called initState');
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print('didChangeDependencies');
-  }
-
-  @override
-  void didUpdateWidget(covariant HomeScreen oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print('didUpdateWidget');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('Build method');
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(
-        child: Column(
-          children: [
-            Text('$count'),
-            TextButton(
-              onPressed: () {
-                count++;
-                print(count);
-                setState(() {});
-              },
-              child: Text('Increment'),
-            ),
-            TextButton(
-              onPressed: () {
-                count--;
-                print(count);
-                setState(() {});
-              },
-              child: Text('Decrement'),
-            ),
-          ],
-        ),
+    return MaterialApp(
+      title: 'Crud App',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue
       ),
+      home: HomeScreen(),
     );
-  }
-
-  @override
-  void deactivate() {
-    print('deactive');
-    super.deactivate();
-  }
-
-  // Important
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    print('dispose');
-    super.dispose();
   }
 }
---
+++ live_class_project/lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:live_class_project/screens/add_new_product_screen.dart';
+import 'package:live_class_project/widgets/product_item.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Product List')),
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ProductItem();
+        },
+        separatorBuilder: (context, index) {
+          return Divider(indent: 70);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddNewProductScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
