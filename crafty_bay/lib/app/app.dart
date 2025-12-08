37f14236748009c757f5af74140a5b18da2b1598
RafatMeraz <razhossain201@gmail.com>
2025-12-08 23:02:31 +0600

    Set up firebase, theme and routes

---
+++ crafty_bay/lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:crafty_bay/app/app_routes.dart';
+import 'package:crafty_bay/app/app_theme.dart';
+import 'package:crafty_bay/features/auth/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatefulWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  State<CraftyBayApp> createState() => _CraftyBayAppState();
+}
+
+class _CraftyBayAppState extends State<CraftyBayApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: SplashScreen.name,
+      onGenerateRoute: AppRoutes.routes,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: .light,
+    );
+  }
+}
