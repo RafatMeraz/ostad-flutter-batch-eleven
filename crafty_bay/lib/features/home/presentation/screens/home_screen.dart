5d24ee624c46737b3475abdd7854b4996571696d
RafatMeraz <razhossain201@gmail.com>
2025-12-14 23:08:19 +0600

    [WIP]: Home screen ui design in progress

--- crafty_bay/lib/features/home/presentation/screens/home_screen.dart
+++ crafty_bay/lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:crafty_bay/app/asset_paths.dart';
+import 'package:crafty_bay/features/home/presentation/widgets/home_carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widgets/circle_icon_button.dart';
+import '../widgets/product_search_field.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +16,37 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+          child: Column(
+            spacing: 16,
+            children: [
+              ProductSearchField(),
+              HomeCarouselSlider()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: SvgPicture.asset(AssetPaths.navLogoSvg),
+      actions: [
+        CircleIconButton(onTap: () {}, icon: Icons.person),
+        const SizedBox(width: 4),
+        CircleIconButton(onTap: () {}, icon: Icons.call),
+        const SizedBox(width: 4),
+        CircleIconButton(
+          onTap: () {},
+          icon: Icons.notifications_active_outlined,
+        ),
+        const SizedBox(width: 4),
+      ],
+    );
   }
 }
