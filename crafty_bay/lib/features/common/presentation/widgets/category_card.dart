209c3d1a051fe1fa33c656b6be9b9717f2fd1aae
RafatMeraz <razhossain201@gmail.com>
2025-12-16 23:12:27 +0600

    Add category list screen ui

---
+++ crafty_bay/lib/features/common/presentation/widgets/category_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../../app/app_colors.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          elevation: 0,
+          color: AppColors.themeColor.withAlpha(30),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Icon(Icons.computer, size: 28, color: AppColors.themeColor),
+          ),
+        ),
+        Text(
+          'Computer',
+          style: TextStyle(
+            fontWeight: .w500,
+            color: AppColors.themeColor,
+            letterSpacing: .6,
+          ),
+        ),
+      ],
+    );
+  }
+}
