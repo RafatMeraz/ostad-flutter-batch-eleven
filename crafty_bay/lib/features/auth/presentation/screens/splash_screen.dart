72d3321df6c3abc7668ec399eb964a409656f9f6
RafatMeraz <razhossain201@gmail.com>
2025-12-23 23:14:40 +0600

    Integrate sign up api

--- crafty_bay/lib/features/auth/presentation/screens/splash_screen.dart
+++ crafty_bay/lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crafty_bay/features/auth/presentation/screens/sign_up_screen.dart';
 import 'package:crafty_bay/features/common/presentation/screens/main_nav_holder_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +24,7 @@ class _SplashScreenState extends State<SplashScreen> {
     await Future.delayed(Duration(seconds: 2));
     Navigator.pushNamedAndRemoveUntil(
       context,
-      MainNavHolderScreen.name,
+      SignUpScreen.name,
       (predicate) => false,
     );
   }
